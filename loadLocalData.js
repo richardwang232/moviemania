0cfc82f2fe4a2f993a7523845f906e91466e8958
Richard <richard.wang.2011@gmail.com>
2013-11-10 22:10:37 -0500

    first commit

---
+++ loadLocalData.js
@@ -0,0 +1,59 @@
+      var movieData = [
+        {title: "Elysium", imgURL: "http://fc01.deviantart.net/fs70/f/2012/189/6/e/elysium_movie_poster_by_dcomp-d56j3bj.jpg"},
+        {title: "World War Z", imgURL: "http://latimesherocomplex.files.wordpress.com/2013/03/world-war-z.jpg"},
+        {title: "Pacific Rim", imgURL: "http://www.impawards.com/2013/posters/pacific_rim_ver3.jpg"},
+        {title: "Monsters University", imgURL: "http://images.fandango.com/images/fandangoblog/MUNTeaserposter900.jpg"},
+        {title: "White House Down", imgURL: "http://www.impawards.com/2013/posters/white_house_down_ver4.jpg"},
+        {title: "Despicable Me 2", imgURL: "http://www.upcoming-movies.com/image/despicable-me-two-movie-poster.jpg"},
+        {title: "The Lone Ranger", imgURL: "http://www.impawards.com/2013/posters/lone_ranger.jpg"},
+        {title: "Grown Ups 2", imgURL: "http://4.bp.blogspot.com/-iQZczN2Lb-4/UUJalia7QoI/AAAAAAAAACA/PN1K-dPekVo/s400/grown_ups_2_new-poster.jpg"},
+        {title: "Fruitvale Station", imgURL: "http://www.impawards.com/2013/posters/fruitvale_station_xlg.jpg"}
+      ];
+      
+      var elysiumTweets = [
+        {content: "OMG SO GOOD"},
+        {content: "Matt Damon!"},
+        {content: "The future looks so scary!"}
+      ];
+      
+      var wwzTweets = [
+        {content: "Brad Pitt!"},
+        {content: "What a horrible resolution to the film"},
+        {content: "The future looks so scary!"}
+      ];
+      
+      var pacificRimTweets = [
+        {content: "I love mechs!"},
+        {content: "When legions of monstrous creatures, known as Kaiju, started rising from the sea, a war began that would take millions of lives and consume humanity’s resources for years on end."},
+        {content: "The future looks so scary!"}       
+      ]
+      
+      var muTweets = [
+        {content: "I love Monsters University!"},
+        {content: "When legions of monstrous creatures, known as Kaiju, started rising from the sea, a war began that would take millions of lives and consume humanity’s resources for years on end."},
+        {content: "The future looks so scary!"}       
+      ];
+
+      var wwdTweets = [
+        {content: "I love White House Down!"},
+        {content: "When legions of monstrous creatures, known as Kaiju, started rising from the sea, a war began that would take millions of lives and consume humanity’s resources for years on end."},
+        {content: "The future looks so scary!"}       
+      ];
+
+      var dm2Tweets = [
+        {content: "I love Despicable Me 2!"},
+        {content: "When legions of monstrous creatures, known as Kaiju, started rising from the sea, a war began that would take millions of lives and consume humanity’s resources for years on end."},
+        {content: "The future looks so scary!"}       
+      ];
+      var tlrTweets = [
+        {content: "I love The Lone Ranger!"}
+      ];
+      var gu2Tweets = [
+        {content: "I love Grown Ups 2!"}
+      ];
+      var fsTweets = [
+        {content: "I love Fruitvale Station!"}
+      ]
+      var movieTweets = {'World War Z':wwzTweets, 'Elysium':elysiumTweets, 'Pacific Rim':pacificRimTweets,
+      "White House Down": wwdTweets, "Monsters University": muTweets, "Despicable Me 2": dm2Tweets, "The Lone Ranger": tlrTweets,
+      "Grown Ups 2": gu2Tweets, "Fruitvale Station": fsTweets};
