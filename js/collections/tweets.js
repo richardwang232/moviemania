69689cf5131da224c9202d358b553cbe4f626c08
Richard Wang <richard@realdirect.com>
2014-05-13 18:26:19 -0400

    moved OAuth2 Bearer token POST to initialize as it only needs to be called once

--- js/collections/tweets.js
+++ js/collections/tweets.js
@@ -6,23 +6,24 @@ define([
 ], function($, _, Backbone, TweetModel) {
 	var Tweets = Backbone.Collection.extend({
 		initialize: function() {
-
+			//this only needs to be done once, get bearer token for OAuth Twitter
+			this.cb = new Codebird;
+			this.cb.setConsumerKey("dJubmPzGfaSiM4z6enh5Pw", "QdulpG2gTkfftg2yaGxYYgbqTwF3BoARsqizRi54rc");
+			this.cb.__call(
+			    "oauth2_token",
+			    {},
+			    function (reply) {
+			        var bearer_token = reply.access_token;
+			        console.log(bearer_token);
+			    }
+			);
 		},
 		sync: function(method, model, options) {
 			var that = this;
 			if (method === 'read') {
 				var title = options.title;
+				
 				//** this is back-end API call code...should put in sync
-				this.cb = new Codebird;
-				this.cb.setConsumerKey("dJubmPzGfaSiM4z6enh5Pw", "QdulpG2gTkfftg2yaGxYYgbqTwF3BoARsqizRi54rc");
-				this.cb.__call(
-				    "oauth2_token",
-				    {},
-				    function (reply) {
-				        var bearer_token = reply.access_token;
-				        console.log(bearer_token);
-				    }
-				);
 				this.cb.__call(
 				"search_tweets",
 				"q="+encodeURIComponent(title)+"&lang=en",
@@ -40,6 +41,7 @@ define([
 				    }
 				    //** set collection models to the new array of tweets
 				    that.reset(tweets);
+//				    return true;
 				},
 				true // this parameter required
 				);
