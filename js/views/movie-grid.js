f0565cb51110d038692de0cbce16288e4d08b00c
Richard Wang <richard@realdirect.com>
2014-06-23 12:18:53 -0400

    added pagination to movie grid view #1

--- js/views/movie-grid.js
+++ js/views/movie-grid.js
@@ -11,7 +11,9 @@ function($, _, Backbone, Mustache, MovieModel, TMDBConfig, MovieGridTemplate) {
   var MovieGrid = Backbone.View.extend({
   	el: '#movies-container',
   	events: {
-
+  		'click .next-page': 'next',
+  		'click .prev-page': 'prev',
+  		'click .page': 'goToPage'
   	},
   	initialize: function() {
   		var that = this;
@@ -20,13 +22,49 @@ function($, _, Backbone, Mustache, MovieModel, TMDBConfig, MovieGridTemplate) {
 		});
   	},
   	render: function() {
+  		var that = this;
+		var pages = [];
+		for (var i=this.collection.state.firstPage; i <= this.collection.state.lastPage; i++) {
+			pages.push({
+				page_number: i
+			})
+		}
   		var context = {
   			movies: this.collection.toJSON(),
 	        base_url : TMDBConfig.get("images").base_url, //** from TMDB /config API call
-	        poster_size : TMDBConfig.get("images").poster_sizes[5]
+	        poster_size : TMDBConfig.get("images").poster_sizes[5],
+			hasPreviousPage: (this.collection.state.currentPage > this.collection.state.firstPage),
+			hasNextPage: (this.collection.state.currentPage < this.collection.state.lastPage),
+			pages: pages,
+			isCurrentPage: function() {
+				return function(val, render) {
+					var page_number = parseInt(render(val),10);
+					if (page_number === that.collection.state.currentPage) {
+						return "<b>" + render(val) + "</b>";
+					}
+					else {
+						return render(val);
+					}
+				}
+			}
   		};
   		this.$el.html(Mustache.render(MovieGridTemplate, context));
-  	}
+  		this.delegateEvents();
+  	},
+  	next: function() {
+  		this.collection.getNextPage();
+  		this.render();
+  	},
+  	prev: function() {
+  		this.collection.getPreviousPage();
+  		this.render();
+  	},
+	goToPage: function(event) {
+		event.preventDefault();
+		var page_number = $(event.target).data('page');
+		this.collection.getPage(page_number);
+		this.render();
+	}
   });
   return MovieGrid;
 });
