ac721db32474e17b1d2c32a4b5b5a72c62271365
Richard Wang <richard@realdirect.com>
2014-06-23 12:15:34 -0400

    added backbone.paginator library #1

--- js/collections/movies.js
+++ js/collections/movies.js
@@ -2,17 +2,23 @@ define([
 	'jquery',
 	'underscore',
 	'backbone',
-	'models/movie'
-], function($, _, Backbone) {
-	var Movies = Backbone.Collection.extend({
-		tmdb_base_url: "",
+	'models/movie',
+	'backbone.paginator',
+], function($, _, Backbone, MovieModel) {
+	var Movies = Backbone.PageableCollection.extend({
+		mode: 'client',
+		model: MovieModel,
 		url: "https://api.themoviedb.org/3/movie/now_playing?api_key=00b1a245c429c285c83f95fbb86180be",
+		tmdb_base_url: "",
+		state: {
+			pageSize: 4
+		},
 		initialize: function() {
 
 		},
 		parse: function(response) {
 			return response.results;
 		}
-      });	
+     });
 	return Movies;
 });
--- js/main.js
+++ js/main.js
@@ -1,17 +1,18 @@
 require.config({
     baseUrl: 'js',
     paths: {
-        jquery: "http://cdnjs.cloudflare.com/ajax/libs/jquery/1.8.2/jquery.min",
-        underscore: "lib/underscore-amd",
-        backbone: "lib/backbone-amd",
-        localstorage: "lib/backbone.localStorage",
-        mustache: "http://cdnjs.cloudflare.com/ajax/libs/mustache.js/0.7.2/mustache.min",
-        sha1: "lib/sha1",
-        codebird: "lib/codebird",
-        less: "http://cdnjs.cloudflare.com/ajax/libs/less.js/1.5.0/less.min",
-        bootstrap: "http://netdna.bootstrapcdn.com/bootstrap/3.0.2/js/bootstrap.min",
-        text: "lib/text",
-        async: "lib/async"
+        "jquery": "http://cdnjs.cloudflare.com/ajax/libs/jquery/1.8.2/jquery.min",
+        "underscore": "lib/underscore-amd",
+        "backbone": "lib/backbone-amd",
+        "localstorage": "lib/backbone.localStorage",
+        "mustache": "http://cdnjs.cloudflare.com/ajax/libs/mustache.js/0.7.2/mustache.min",
+        "sha1": "lib/sha1",
+        "codebird": "lib/codebird",
+        "less": "http://cdnjs.cloudflare.com/ajax/libs/less.js/1.5.0/less.min",
+        "bootstrap": "http://netdna.bootstrapcdn.com/bootstrap/3.0.2/js/bootstrap.min",
+        "text": "lib/text",
+        "async": "lib/async",
+        "backbone.paginator": "lib/backbone.paginator"
     },
     shim: {
       bootstrap: ['jquery']
@@ -41,7 +42,7 @@ require([
 function($, _, Backbone, MovieModel, TMDBConfig, MoviesCollection, SavedMoviesCollection, MovieGridView, MoviesView, TweetsView, SavedMoviesView, SavedMoviesTemplate, Mustache) {
 
   $.when(TMDBConfig.fetch()).then(function() {
-    var savedMovies = new SavedMoviesCollection();
+//    var savedMovies = new SavedMoviesCollection();
     var movies = new MoviesCollection();
 
     var moviesView = new MovieGridView({collection: movies});
